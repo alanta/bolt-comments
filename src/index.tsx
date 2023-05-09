85ab11b8e2a5127e1afc9226b7c67af5ebee81ca
Marnix van Valen <m.vanvalen@4dotnet.nl>
2023-05-09 15:32:00 +0200

    👷 Switch to Parcel for building
    react-scripts is a security risk

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './assets/js/vendor/jquery.min.js'
+import './assets/js/vendor/popper.min.js'
+import './assets/js/vendor/bootstrap.min.js'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,8 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
